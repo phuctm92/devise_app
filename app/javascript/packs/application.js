4135a07bf4c4db50fc384cf172c774214b9be6ec
Phuc Thai <phuc.tm4192@gmail.com>
2021-03-10 16:48:26 +0700

    Refactor bootstrap setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-import "bootstrap"
+import 'bootstrap/dist/js/bootstrap'
 import "@fortawesome/fontawesome-free/css/all"
+import 'stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
