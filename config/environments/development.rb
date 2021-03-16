0d0ef3368e1b2ab9537f4dbc400c6ee03a08eebd
Phuc Thai <phuc.tm4192@gmail.com>
2021-03-16 21:46:40 +0700

    setup mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 9999 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # setup mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { host: 'localhost', port: 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
