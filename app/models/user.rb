4b898b6e0fd99e25155607ecfe0e83cbf99ae091
Unknown <phuctm@zigexn.vn>
2021-03-19 17:17:54 +0700

    Login with Username or Email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  attr_accessor :login
+
+  # https://web-crunch.com/posts/devise-login-with-username-email
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).find_by('lower(username) = :value OR lower(email) = :value', value: login.strip.downcase)
+  end
 end
